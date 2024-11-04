a9e64289590706ab232554df701ce6062fbcd3a7
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2024-11-04 17:59:05 +0100

    Actualizacion del componente AddCategory

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,32 +1,34 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 export const AddCategory = ({ onNewCategory }) => {
 
-    const [ inputValue, setInputValue ] = useState('');
+    const [inputValue, setInputValue] = useState('');
 
-    const onInputChange = (target) => {
-        setInputValue(target.value);
-    }
+    const onInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
 
-    const onSubmit = ( event ) => {
+    const onSubmit = (event) => {
         event.preventDefault();
         if (inputValue.trim().length <= 1) return;
 
-        //setCategories( categories => [inputValue, ...categories]);
-        onNewCategory( inputValue.trim());
+        onNewCategory(inputValue.trim());
         setInputValue('');
-        
-       
-    }
+    };
 
-  return (
-    <form onSubmit={ onSubmit }>
-        <input 
-            type="text"
-            placeholder="buscar gifs"
-            value={inputValue}
-            onChange={onInputChange}
-        />
-    </form>
-  )
-}
+    return (
+        <form onSubmit={onSubmit}>
+            <input 
+                type="text"
+                placeholder="Buscar gifs"
+                value={inputValue}
+                onChange={onInputChange}
+            />
+        </form>
+    );
+};
+
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired
+};
